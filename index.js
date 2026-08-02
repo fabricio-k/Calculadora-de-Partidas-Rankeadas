ca1b1633622350cd1cd82b9a5121da7f3cbf206b
fabricio-k <fabriciosn9@gmail.com>
2026-08-02 00:23:59 -0300

    Adicionando IF/ELSE

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 console.log("teste")
 
 let resultado = contagem()
+let nivel;
 console.log(resultado)
 
 
 function contagem(){
-    let vitorias = 7
-    let derrotas = 5
+    let vitorias = 100000
+    let derrotas = 10
     let diminui = vitorias - derrotas
     return diminui
 }
 
+if(resultado <= 10){
+    nivel = "Ferro"
+}else if(resultado >= 101  ){
+    nivel = "imortal"
+}
+
+console.log(`O Herói tem saldo de ${resultado} está no nível de ${nivel}`)
